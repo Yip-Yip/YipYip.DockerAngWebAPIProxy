48dfb462db184621648807899c63abe5ae42d8e8
Justin Oehlmann :D <justin.oehlmann@pwc.com>
2024-11-19 13:22:50 +0100

    init

---
+++ YipYip.DockerWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YipYip.DockerWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            string computerName = Environment.GetEnvironmentVariable("COMPUTERNAME") ?? "None";
+            string hostName = Environment.GetEnvironmentVariable("HOSTNAME") ?? "None";
+            string processId = Process.GetCurrentProcess().Id.ToString();
+            string processName = Process.GetCurrentProcess().ProcessName;
+            string machineName = Process.GetCurrentProcess().MachineName;
+
+            ColorConsole.WriteLine("'Get' Request received by '{0}' ", "cyan", hostName);
+
+            return new string[]
+            {
+                "Computer Name: " + computerName, "Host Name: " + hostName, "Machine Name: " + machineName,
+                "Process Id: " + processId, "Process Name: " + processName
+            };
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ YipYip.DockerWebAPI/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace YipYip.DockerWebAPI
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Configure Kestrel to listen on any IP address
+            // builder.WebHost.ConfigureKestrel(serverOptions =>
+            // {
+            //     serverOptions.ListenAnyIP(8080); // Listen for HTTP traffic on port 5000
+
+            // });
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            //app.UseAuthorization();
+
+            // global cors policy
+            app.UseCors(policy =>
+            {
+                policy.SetIsOriginAllowed(origin => true) // Allow any origin
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All,
+                AllowedHosts = new List<string>()
+            });
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
